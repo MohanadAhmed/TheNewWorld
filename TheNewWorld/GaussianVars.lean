67a83b7cb28782d0ac749b8b810f3169ab1a11ff
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-07-31 15:00:05 +0300

    all working without mean

--- TheNewWorld/GaussianVars.lean
+++ TheNewWorld/GaussianVars.lean
@@ -3,14 +3,14 @@ import Mathlib
 
 open MeasureTheory Real
 
-noncomputable def fμσ (μ : ℝ) (σ : ℝ) := fun x => ENNReal.ofReal (Real.sqrt (π/2 / (2*σ^2))⁻¹ * Real.exp (-2*(x)^2 / (2*σ^2)))
+noncomputable def fμσ (μ : ℝ) (σ : ℝ) := fun x => ENNReal.ofReal (Real.sqrt (π/(2 / (2*σ^2)))⁻¹ * Real.exp (-2*(x)^2 / (2*σ^2)))
 -- noncomputable def fμσ (μ : ℝ) (σ : ℝ) := fun x => ENNReal.ofReal (Real.sqrt (π/2)⁻¹ * Real.exp (-2*(x)^2))
 
 noncomputable def gaussianMeasure (μ : ℝ) (σ : ℝ) : MeasureTheory.Measure ℝ := by
   apply MeasureTheory.Measure.withDensity volume (fμσ μ σ)
 
 -- set_option pp.all true
-lemma gaussianMeasure_isProbability (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) : 
+lemma gaussianMeasure_isProbabilityMeasure (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) : 
     IsProbabilityMeasure (gaussianMeasure μ σ) := by
   unfold gaussianMeasure
   refine' {measure_univ := _}
@@ -21,16 +21,24 @@ lemma gaussianMeasure_isProbability (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) :
   rw [← ofReal_integral_eq_lintegral_ofReal]
   · rw [← ENNReal.ofReal_one, ENNReal.ofReal_eq_ofReal_iff _ zero_le_one]
     simp_rw [←smul_eq_mul, integral_smul, smul_eq_mul, mul_div_right_comm, neg_div]
-    have hn1 : 0 < (π / 2 / (2 * σ^2)) := by
-      simp only [rpow_two]
-      apply (div_pos (div_pos pi_pos two_pos) (mul_pos two_pos (pow_pos hσ 2)))
-    have hn2 : (π / 2 / (2 * σ^2)) ≠ 0 := by
-      apply ne_of_gt
-      exact hn1
-    
-    rw [ integral_gaussian, ← sqrt_mul, sqrt_eq_one]
-    simp only [rpow_two]
-    
+    rw [ integral_gaussian, ← sqrt_mul, sqrt_eq_one, inv_mul_cancel]
+    apply div_ne_zero (pi_ne_zero) (div_ne_zero two_ne_zero (ne_of_gt (mul_pos two_pos (pow_pos hσ _))))
+    rw [inv_nonneg]
+    apply div_nonneg (le_of_lt pi_pos) (le_of_lt _)
+    apply div_pos two_pos (mul_pos two_pos (pow_pos hσ _))
+    apply integral_nonneg
+    · intro x
+      simpa [Pi.zero_apply] using (mul_nonneg (sqrt_nonneg _) (le_of_lt (exp_pos _)))
+  apply Integrable.const_mul
+  simp_rw [mul_div_right_comm, neg_div]
+  apply integrable_exp_neg_mul_sq (div_pos two_pos (mul_pos two_pos (pow_pos hσ _)))
+
+  · unfold Filter.EventuallyLE Filter.Eventually
+    dsimp only [Pi.zero_apply]
+    apply Filter.univ_mem'
+    intro x
+    simp only [Set.mem_setOf_eq]
+    apply mul_nonneg (sqrt_nonneg _) (le_of_lt (exp_pos _))
     
     
     
@@ -41,7 +49,8 @@ lemma gaussianMeasure_isProbability (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) :
   --   · intro x
   --     simpa [Pi.zero_apply] using (mul_nonneg (sqrt_nonneg _) (le_of_lt (exp_pos _)))
 
-  -- · apply Integrable.const_mul (integrable_exp_neg_mul_sq two_pos) _
+  -- · apply Integrable.const_mul
+    
 
   -- · unfold Filter.EventuallyLE Filter.Eventually
   --   dsimp only [Pi.zero_apply]
