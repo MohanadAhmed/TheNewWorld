7f18999c755f1d2b0fb0d7eaac7be7d17553de4d
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-08-02 01:14:43 +0300

    Some Cleanup

--- TheNewWorld/GaussianVars.lean
+++ TheNewWorld/GaussianVars.lean
@@ -11,15 +11,18 @@ noncomputable def gaussianMeasure (μ : ℝ) (σ : ℝ) : MeasureTheory.Measure 
 lemma gaussianMeasure_isProbabilityMeasure (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) : 
     IsProbabilityMeasure (gaussianMeasure μ σ) := by
   have h1 : ∀ (a : ℝ), -a ^ 2 / (2*σ^2) = -(1/(2*σ^2)) * (a ^ 2) := by intro; ring;
-  have h2 : ∀ (x : ℝ), -(x ^ 2)/(2*σ^2) = (-(2*σ^2)⁻¹ * (x ^ 2)) := by intro; ring;
-  
+  have σ2_pos : 0 < (2*σ^2)⁻¹ := by 
+    simp only [rpow_two]; apply inv_pos.2 (mul_pos two_pos (pow_pos hσ _))
+    done
+  let fσ := (fun x => ENNReal.ofReal (Real.exp (-(x^2 / (2*σ^2)))))
+
   unfold gaussianMeasure
   refine' {measure_univ := _}
   simp only [MeasurableSet.univ, withDensity_apply, Measure.restrict_univ]
   unfold fμσ
   -- simp only [Real.rpow_two]
   rw [← ofReal_integral_eq_lintegral_ofReal] -- Generates three side goals
-  -- Integral of function = 1
+  -- SUBGOAL 1: Integral of function = 1
   · rw [← ENNReal.ofReal_one, ENNReal.ofReal_eq_ofReal_iff _ zero_le_one]
     simp_rw [sub_eq_neg_add, add_comm]
     rw [integral_add_right_eq_self (fun x => (Real.sqrt (π/( 1 / (2*σ^2)))⁻¹ * Real.exp (-(x)^2 / (2*σ^2)))) (-μ)]
@@ -32,48 +35,29 @@ lemma gaussianMeasure_isProbabilityMeasure (μ : ℝ) (σ : ℝ)(hσ: 0 < σ) :
     apply integral_nonneg
     · intro x
       simpa [Pi.zero_apply] using (mul_nonneg (sqrt_nonneg _) (le_of_lt (exp_pos _)))
-  -- Lebesgue Integral is finite
+
+  -- SUBGOAL 2:  Lebesgue Integral is finite
   · apply Integrable.const_mul
     simp_rw [mul_div_right_comm, neg_div]
     unfold Integrable
     constructor
-    · apply Measurable.aestronglyMeasurable
-      apply Continuous.measurable
-      apply Continuous.comp Real.continuous_exp
-      apply Continuous.neg
-      apply Continuous.div
-      simp only [rpow_two]
-      apply Continuous.pow
-      apply Continuous.sub
-      apply continuous_id
-      apply continuous_const
-      apply continuous_const
-      · intro
-        simp only [rpow_two, ne_eq, mul_eq_zero, OfNat.ofNat_ne_zero, zero_lt_two, pow_eq_zero_iff, false_or]
-        apply ne_of_gt hσ
+    -- Continuity of the gaussian function
+    simp only [rpow_two]
+    apply Measurable.aestronglyMeasurable (Continuous.measurable _)
+    apply Continuous.comp Real.continuous_exp (Continuous.neg (Continuous.div_const _ _))
+    apply Continuous.pow (Continuous.sub continuous_id continuous_const)
+    -- Finiteness of the Gaussian Integral
     unfold HasFiniteIntegral
-    dsimp
-    rw [MeasureTheory.lintegral_nnnorm_eq_of_nonneg]
     simp_rw [← rpow_two]
-    rw [MeasureTheory.lintegral_sub_right_eq_self (fun x => ENNReal.ofReal (Real.exp (-(x^2 / (2*σ^2))))) μ]
-    rw [← MeasureTheory.lintegral_nnnorm_eq_of_nonneg]
+    rw [lintegral_nnnorm_eq_of_nonneg, lintegral_sub_right_eq_self fσ μ, 
+      ← lintegral_nnnorm_eq_of_nonneg]
     apply Integrable.hasFiniteIntegral
     simp_rw [div_eq_inv_mul, ← neg_mul]
-    set c := (2*σ^2)⁻¹
-    have : 0 < (2*σ^2)⁻¹ := by
-      apply inv_pos.2
-      apply mul_pos two_pos
-      simp only [rpow_two]
-      apply (pow_pos hσ _)
-    convert integrable_exp_neg_mul_sq this
+    convert integrable_exp_neg_mul_sq σ2_pos
     simp only [rpow_two]
-    · intro x
-      simp only [Pi.zero_apply, rpow_two]
-      apply (le_of_lt (exp_pos _))
-    · intro x
-      simp only [Pi.zero_apply, rpow_two]
-      apply (le_of_lt (exp_pos _))
-  -- Something about measurability!!!
+    all_goals ( intro x; simp only [Pi.zero_apply, rpow_two]; apply (le_of_lt (exp_pos _)))
+
+  -- SUBGOAL 3:  Something about measurability!!!
   · unfold Filter.EventuallyLE Filter.Eventually
     dsimp only [Pi.zero_apply]
     apply Filter.univ_mem'
